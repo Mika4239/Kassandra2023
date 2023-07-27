ababa08068ed012ddee6bceedae4c80f400f9f3a
Mika <mikush.carmel@gmail.com>
2023-07-27 12:17:50 +0300

    feat(router): added select match to router

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import SelectMatch from "../views/selectMatch/selectMatch";
 
 const router = createBrowserRouter([
+    {
+        path: '/select',
+        Component: SelectMatch
+    }
 ]);
 
 export default router;
