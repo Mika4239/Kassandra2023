07b08801aea9b71f21fb2bf78cfac17cea01c397
Mika <mikush.carmel@gmail.com>
2023-08-04 15:40:44 +0300

    feat(addTeamDialog): added dialog for adding new team

--- frontend/src/views/teams/teams.tsx
+++ frontend/src/views/teams/teams.tsx
@@ -5,6 +5,7 @@ import axios from "../../axios";
 import useStyles from "./teamsStyles";
 import { Button } from "@mui/material";
 import GroupIcon from "@mui/icons-material/Group";
+import AddTeamDialog from "../../components/addGroupDialog/addTeamDialog";
 
 const TEAMS = "Teams";
 const ADD_TEAM = "Add Team";
@@ -12,6 +13,7 @@ const ADD_TEAM = "Add Team";
 const Teams: React.FC = () => {
   const { classes } = useStyles();
 
+  const [open, setOpen] = useState<boolean>(false);
   const [teams, setTeams] = useState<Team[]>([]);
 
   useEffect(() => {
@@ -23,10 +25,15 @@ const Teams: React.FC = () => {
       <NavBar />
       <div className={classes.teamsPage}>
         <h1 className={classes.title}>{TEAMS}</h1>
-        <Button variant="contained" className={classes.addGroup}>
+        <Button
+          variant="contained"
+          className={classes.addGroup}
+          onClick={() => setOpen(true)}
+        >
           {ADD_TEAM}
           <GroupIcon className={classes.groupIcon} />
         </Button>
+        <AddTeamDialog open={open} setOpen={setOpen} setTeams={setTeams} />
         {teams.map((team) => (
           <TeamCard team={team} />
         ))}
