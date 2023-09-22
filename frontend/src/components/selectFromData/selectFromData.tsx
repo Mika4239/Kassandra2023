b723ccf0de321be17e95497f8e7a0625dd3837f1
Mika <mikush.carmel@gmail.com>
2023-09-22 18:51:08 +0300

    feat(selectFromData): added translation to select match in scouting

--- frontend/src/components/selectFromData/selectFromData.tsx
+++ frontend/src/components/selectFromData/selectFromData.tsx
@@ -12,6 +12,22 @@ const SelectFromData: React.FC<SelectFromDataProps> = (props) => {
   const { classes } = useStyles();
   const { name, data } = props;
 
+  const TbaKeys =  {
+    "qm": "qualification match",
+    "ef": "eighth finals",
+    "qf": "quarterfinals",
+    "sf": "semifinals",
+    "f": "finals",
+    "m": "match",
+    "frc": ""
+  };
+  
+  const translateTbaKey = (key: string) => {
+    const tbaKeyArr = (key.split("_")[1] || key).split(/(\d+)/);
+    // all the even places in the array are strings and odds places are numbers, so we need to translate only the strings
+    return tbaKeyArr.map((matchKey, index) => index % 2 == 0 ? TbaKeys[matchKey as keyof(typeof TbaKeys)] : matchKey).join(" ");
+  }
+
   const chosen = useAppSelector(state => state.matchData[name as keyof SelectMatchState]);
   const dispatch = useDispatch();
 
@@ -25,7 +41,7 @@ const SelectFromData: React.FC<SelectFromDataProps> = (props) => {
         onChange={(e) => dispatch(setMatchTeam({name: name, input: e.target.value}))}
       >
         {data.map((item, index) => (
-          <MenuItem key={index} value={item}>{item}</MenuItem>
+          <MenuItem key={index} value={item}>{translateTbaKey(item)}</MenuItem>
         ))}
       </Select>
     </FormControl>
