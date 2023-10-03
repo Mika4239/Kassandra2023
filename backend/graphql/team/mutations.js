6e1629486e5751a227f96735740a2e01cb886fa1
Mika <mikush.carmel@gmail.com>
2023-10-03 21:02:31 +0300

    feat(backend): removed backend

--- backend/graphql/team/mutations.js
+++
@@ -1,7 +0,0 @@
-module.exports.createTeam = `
-mutation createTeam($name: String!, $number: Int!, $image: String, $description: String, $admin: [String!]!) {
-    createTeam(input: {name: $name, number: $number, description: $description, image: $image, admin: $admin}) {
-      id
-    }
-  }
-`;
